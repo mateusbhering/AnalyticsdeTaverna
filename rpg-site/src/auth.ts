331ca277800793565612d62b2d249ea8b3060918
Mateus Bhering <mateusbbhering@gmail.com>
2026-05-08 17:11:14 -0300

    Add Tsunokaway as admin

--- rpg-site/src/auth.ts
+++ rpg-site/src/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
-const ADMIN_USERNAMES = ["mateusbhering", "juliacrws"];
+const ADMIN_USERNAMES = ["mateusbhering", "juliacrws", "Tsunokaway"];
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
