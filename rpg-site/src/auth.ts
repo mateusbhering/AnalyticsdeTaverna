0acfdbf03f8c09aa01c2d8e81ce5ed0e4626e2e5
Mateus Bhering <mateusbbhering@gmail.com>
2026-06-12 15:31:41 -0300

    add gui ladeira adm

--- rpg-site/src/auth.ts
+++ rpg-site/src/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
-const ADMIN_USERNAMES = ["mateusbhering", "juliacrws", "Tsunokaway", "oipimenta", "marianakuramitsu"];
+const ADMIN_USERNAMES = ["mateusbhering", "juliacrws", "Tsunokaway", "oipimenta", "marianakuramitsu", "Ladeira26"];
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
